573c96083cc9626f1bbcebe25d172878b5c1adb3
desantel <ecdesantis21@gmail.com>
2021-09-13 21:45:55 -0400

    information showing up on page.  date not working.

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
+let card = document.querySelector('card');
+
 //url for api key
-const url = 'https://api.nasa.gov/planetary/apod?api_key=';
+const url = "https://api.nasa.gov/planetary/apod?api_key=";
 
 //calls api key from config, keeps key confidential
 const api_key = config.API_KEY;
 
-
+//function to fetch data from url, logs data and displays
 const fetchData = async () => {
-    try {
-        const res = await fetch(`${url}${api_key}`);
-        const data = await res.json();
-        console.log(data)
-    } catch (err) {
-        console.log(err);
-    };
+  try {
+    const res = await fetch(`${url}${api_key}`);
+    const data = await res.json();
+    console.log(data);
+    console.log(data.date)
+    disData(data)
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-let disData = data => {
-    document.getElementById('title').textContent = data.title;
-    document.getElementById('date').textContent = data.date;
-    document.getElementById('picture').textContent = data.hdurl;
-    document.getElementById('description').textContent = data.explanation;
-}
+//function to display to proper areas on page (date not working???)
+let disData = (data) => {
+  document.getElementById("title").textContent = data.title;
+  document.getElementById("date").textContent = data.date;
+  document.getElementById("picture").src = data.hdurl;
+  document.getElementById("description").textContent = data.explanation;
+  document.getElementById('person').textContent = data.copyright;
+};
 
+//function to activate like button, 
+const likeBut = () => {
+
+}
 
 // let datePicker = () => {
 //     $(input[name = "daterange"]).daterangepicker({
